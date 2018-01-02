25beeec4e265ff3be2a2e02f2cffc934bc1dcc66
zhao1157 <zhao.1157@osu.edu>
2018-01-02 13:25:27 -0500

    Update extract_data.sh

--- Extract_Scores/extract_data.sh
+++ Extract_Scores/extract_data.sh
@@ -1,6 +1,7 @@
 #! /bin/awk -f
 
-#This script is to extract the students' scores of an exam and store them into a file for gnuplot later.
+#This script is to extract the students' scores of an exam and store the scores with the associated number of students who get the same 
+#score for gnuplot later.
 
 #Read the number of students took the exam.
 NR == 16 \
